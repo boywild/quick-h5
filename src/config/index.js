c21172f9b58b77344c7ac9e4df001510ddd5158b
chentian <chentian@kayak.com>
2022-03-29 17:55:05 +0800

    项目初始化

---
+++ src/config/index.js
@@ -0,0 +1,43 @@
+/**
+ * 公共配置文件
+ */
+
+const configDict = {
+	development: {
+		baseURL: 'http://localhost:4000'
+	},
+	production: {
+		baseURL: 'http://localhost:4000'
+	}
+}
+
+const currentConfigKey = process.env.NODE_ENV
+const isDev = process.env.NODE_ENV === 'development'
+const isTest = process.env.NODE_ENV === 'development'
+
+const configObj = {
+	isDevelop: (isDev || isTest),
+	...configDict[currentConfigKey],
+	// h5模式宽高
+	canvasH5Width: 375,
+	canvasH5Height: 644,
+	pageModeList: [{
+		value: 'h5',
+		label: 'H5',
+		disabled: false
+	}, {
+		value: 'longPage',
+		label: '长页H5',
+		disabled: false
+	}, {
+		name: 'relativePage',
+		label: '排版图文',
+		disabled: true
+	}, {
+		value: 'pc',
+		label: 'PC页面',
+		disabled: true
+	}]
+}
+
+export default configObj
---
+++ vue.config.js
@@ -0,0 +1,58 @@
+const path = require("path");
+const fs = require("fs");
+
+let devServer = {
+  proxy: {
+    // 代理
+    "/": {
+      target: "http://localhost:4000", //设置你调用的接口域名和端口号 别忘了加http
+      ws: false,
+      changeOrigin: true, // needed for virtual hosted sites
+      pathRewrite: {},
+    },
+  },
+};
+
+module.exports = {
+  devServer: devServer,
+  // 输出文件目录
+  assetsDir: "static",
+  // 修改 pages 入口
+  pages: {
+    index: {
+      entry: "src/main.js", // 入口
+      template: "public/index.html", // 模板
+      filename: "index.html", // 输出文件
+    },
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        // @/ 是 src/ 的别名
+        data: fs.readFileSync(
+          path.resolve(__dirname, `./src/common/styles/variables.scss`),
+          "utf-8"
+        ), // 公共变量文件注入
+      },
+    },
+  },
+  // 扩展 webpack 配置
+  chainWebpack: (config) => {
+    // @ 默认指向 src 目录，这里要改成 examples
+    // 另外也可以新增一个 ~ 指向 packages
+    config.resolve.alias
+      .set("@", path.resolve("src"))
+      .set("@src", path.resolve("src"))
+      .set("@plugins", path.resolve("plugins"));
+    config.module
+      .rule("js")
+      .include.add(/engine-template/)
+      .end()
+      .use("babel")
+      .loader("babel-loader")
+      .tap((options) => {
+        // 修改它的选项...
+        return options;
+      });
+  },
+};
